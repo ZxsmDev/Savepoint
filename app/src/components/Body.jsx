71ec438dcf4717fbebfb1be58990d2be7f8bd278
ZxsmDev <zxsm.dev@gmail.com>
2026-02-10 18:32:50 -0500

    Add Body component with welcome message

---
+++ app/src/components/Body.jsx
@@ -0,0 +1,14 @@
+export default function Body() {
+  return (
+    <>
+      <div className="bg-linear-to-br from-[#121212] to-[#090909] flex flex-col items-center justify-center gap-4 p-4 rounded-lg shadow-md h-full w-full">
+        <h1 className="text-3xl font-bold">Welcome to Savepoint!</h1>
+        <p className="text-lg text-center">
+          Savepoint is a gaming platform that allows you to track your game
+          progress, connect with friends, and discover new games to purchase.
+          Join us today and level up your gaming experience!
+        </p>
+      </div>
+    </>
+  );
+}
